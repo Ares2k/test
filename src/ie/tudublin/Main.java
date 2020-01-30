5baee9f6002cab34622c76886c0c6f4d999a212b
Timur Sultanov <Timursultanov00@gmail.com>
2020-01-30 09:53:52 +0000

    initla version

---
+++ src/ie/tudublin/Main.java
@@ -0,0 +1,15 @@
+package ie.tudublin;
+
+public class Main {
+
+    public static void main(string[] args) {
+
+        for(int i=0; i<args.length; i++) {
+            System.out.println(args[i]);
+        }
+
+        for(String s:args) {
+            System.out.println(s);
+        }
+    }
+}
